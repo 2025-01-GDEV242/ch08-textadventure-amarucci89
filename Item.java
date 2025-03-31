9e747af8a71b9a1e72e1cf3caa56ef6f59bbd208
amarucci89 <alexmarucci89@gmail.com>
2025-03-31 12:18:52 -0400

    Created the Item class

---
+++ Item.java
@@ -0,0 +1,34 @@
+
+/**Lab 6 - Super Text Adventure
+ * This class stores the details of items that can be found in a room.
+ * Some of the details include a description of the item and it's weight.
+ *
+ * @author Alessandro Marucci
+ * @version 2025.03.31
+ */
+public class Item
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
