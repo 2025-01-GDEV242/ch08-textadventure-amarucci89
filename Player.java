df2b2963ed4b65e173c54aaea22f87aabd60b73d
amarucci89 <alexmarucci89@gmail.com>
2025-03-31 22:13:04 -0400

    Created a Player class

---
+++ Player.java
@@ -0,0 +1,33 @@
+
+/** Class Player
+ * This class holds information about a player.
+ *
+ * @author Alessandro Marucci
+ * @version 2025.03.31
+ */
+public class Player
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
